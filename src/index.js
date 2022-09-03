63838b7fdf1967d3b3d063522cfe3684fcd78463
Pulkit Shrivastava <pulkitshrivastava9@gmail.com>
2022-09-03 14:16:25 +0530

    component tree

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDom from "react-dom";
+import './index.css';
 
 function BookList(){
   return (
-    <section>
+    <section className="booklist">
       <Book/>
       <Book/>
       <Book/>
@@ -14,7 +15,7 @@ function BookList(){
 }
 
 const Book = ()=>{
-  return <article>
+  return <article className="book">
     <BookImage/>
     <BookTitle/>
     <BookAuthor/>
@@ -23,6 +24,6 @@ const Book = ()=>{
 
 const BookImage=()=><img src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/A1BrXFzlO-L._AC_UL480_QL65_.jpg"></img>
 const BookTitle=()=><h1>The Famous Five</h1>
-const BookAuthor=()=><h4>Enid Blyton, Claude Voilier, Eileen Soper</h4>
+const BookAuthor=()=><h4 style={{color:'#617d98',fontSize: '0.75rem', margintop: '0.25rem'}}>Enid Blyton, Claude Voilier, Eileen Soper</h4>
 
 ReactDom.render(<BookList/>,document.getElementById('root'));
